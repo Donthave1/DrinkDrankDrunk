59a805b06a11f64ddc4f6fec1d86c730b24888e3
Ed Chen <eddychen0104@gmail.com>
2018-12-04 20:29:02 -0800

    route correction

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ def index():
 
 
 # Add any other routes here
+@app.route("/budget")
+def budgetpage():
+   # """Return the homepage."""
+    return render_template("budget.html")
+
+@app.route("/drunkeness")
+def drunkenesspage():
+   # """Return the homepage."""
+    return render_template("drunkeness.html")
+
+@app.route("/food")
+def foodpage():
+   # """Return the homepage."""
+    return render_template("food.html")
 
 
 if __name__ == "__main__":
