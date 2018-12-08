d52fe2662df9e1a83179c725b0048d26967e535d
Peter Liu <yuhsul1@uci.edu>
2018-12-08 15:43:35 -0800

    d3 graph setup completed

--- static/js/budget.js
+++ static/js/budget.js
@@ -9,14 +9,6 @@ price_range.forEach((price) => {
   option.text(price)
 });
 
-
-// function init() {
-//   // Use the list of sample names to populate the select options
-//   priceDropdown.on("optionChanged", function(selected_budget){
-//     d3.json("price/"+selected_budget).then(buildScatter(data))
-//   })
-// }
-
 function optionChanged(selected_budget){
   var url = `/price/${selected_budget}`;
 
@@ -25,36 +17,111 @@ function optionChanged(selected_budget){
   })
 }
 
+function makeResponsive() {
+  const svgArea = d3.select("body").select("svg");
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+}
 
 
 
-function buildScatter(rofl){
-  xData = []
-  yData = []
 
-  rofl.forEach(function(data) {
-    data.price = +data.price
-    data.review = +data.review
-    xData.push(data.price)
-    yData.push(data.review)
+function buildScatter(hairData){
+
+  const svgWidth = 750;
+  const svgHeight = 500;
+
+  const margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+    };
+
+  const height = svgHeight - margin.top - margin.bottom;
+  const width = svgWidth - margin.left - margin.right;
+
+
+  // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+  const svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+  const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+
+  // Import Data
+    // Step 2: Create scale functions
+  // ==============================
+  let xScale = d3.scaleLinear()
+  .domain([d3.min(hairData, d=> d.review) -5, d3.max(hairData, d => d.review)])
+  .range([0, width]);
+  const yScale = d3.scaleLinear()
+  .domain([d3.min(hairData, d => d.price), d3.max(hairData, d => d.price)])
+  .range([height, 0]);
+  // Step 3: Create axis functions
+  // ==============================
+  const bottomAxis = d3.axisBottom(xScale);
+  const leftAxis = d3.axisLeft(yScale);
+
+
+  // Step 4: Append Axes to the chart
+  // ==============================
+  chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
+  chartGroup.append("g")
+  .call(leftAxis);
+
+  // Apped Axis Label
+  chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 40)
+  .attr("x", 0 - (height / 2))
+  .attr("dy", "1em")
+  .text("Price of the Wine");
+  chartGroup.append("text")
+  .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+  .attr("class", "axisText")
+  .text("Professional Review Score");
+  // Step 5: Create Circles
+  // ==============================
+  hairData.forEach(function(data){
+    data.price = +data.price;
+    data.review = +data.review;
+  })
+    
+  let circlesGroup = chartGroup.selectAll("circle")
+    .data(hairData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.review))
+    .attr("cy", d => yScale(d.price))
+    .attr("r", "5")
+    .attr("fill", "purple")
+    .attr("opacity", "0.5");
+    // Step 6: Initialize tool tip
+    // ==============================
+    let toolTip = d3.tip()
+    .offset([80, -60])
+    .html(function(data) {
+    return `${data.wine_name}<br />
+    Wine Price: ${data.price}<br /> 
+    Wine Review: ${data.review}`;
     });
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+    toolTip.show(data, this);
+    })
+    .on("mouseout", function(data) {
+    toolTip.hide(data);
+    });
+    // Create axes labels
 
-  console.log(xData)
-  console.log(yData)
-
-  const trace1 = {
-    x:xData,
-    y:yData,
-    mode:"markers",
-    type: "scatter",
-    name: "Review vs Price",
-  }
-
-  const data = [trace1]
-  const layout ={
-    title: "Review vs Price",
-    xaxis: {title: "Price"},
-    yaxis: {title: "Review"}
-  }
-  Plotly.newPlot("scatter", data, layout)
 }
