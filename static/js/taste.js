3ca56e572f13b08d85f40c2584554022caaad694
Ed Chen <eddychen0104@gmail.com>
2018-12-10 23:51:38 -0800

    update taste.js function

--- static/js/taste.js
+++ static/js/taste.js
@@ -11,9 +11,9 @@ function optionChanged(selected_taste){
         var url = `/taste/${selected_taste}`;
         console.log("Taste endpoint", url);
 
-        d3.json(url).then(function(response, error){
-            if(error) throw error;
-            console.log(response[0])
+        d3.json(url, function(error, response){
+            // if(error) throw error;
+            // console.log(response[0])
             buildTable(response)    
       })
     }
