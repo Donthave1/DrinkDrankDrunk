65ee3ccce76067f5fa6d55d83e20fdc807620b78
Ed Chen <eddychen0104@gmail.com>
2018-12-10 23:27:25 -0800

    stef branch merge

--- app.py
+++ app.py
@@ -62,8 +62,116 @@ def winebudget(selected_budget):
 
 @app.route("/taste")
 def tastepage():
-   # """Return the homepage."""
-    return render_template("taste.html")
+    conn = 'mongodb://test:password1@ds123444.mlab.com:23444/heroku_3t530jfl'
+    client = pymongo.MongoClient(conn)
+
+    #extract database to DB
+    db = client.heroku_3t530jfl
+
+    df_taste = pd.DataFrame(list(db.taste.find({})))
+    df_taste = df_taste.drop(columns='_id')
+
+    #pull out the tasting notes for the dropdown
+    notes_df = pd.DataFrame(df_taste.tasting_notes.values.tolist())
+    #get rid of the duplicates in the dataframe
+    check_df = notes_df.drop_duplicates(keep='first',inplace=False)
+    check_df = check_df.reset_index()
+
+    s1 = pd.Series(data=check_df[0].dropna())
+    s2 = pd.Series(data=check_df[1].dropna())
+    s3 = pd.Series(data=check_df[2].dropna())
+    s4 = pd.Series(data=check_df[3].dropna())
+    s5 = pd.Series(data=check_df[4].dropna())
+    s6 = pd.Series(data=check_df[5].dropna())
+    s7 = pd.Series(data=check_df[6].dropna())
+    s8 = pd.Series(data=check_df[7].dropna())
+
+    tastes_df = pd.DataFrame(np.hstack((s1.values, s2.values, s3.values, s4.values, s5.values, s6.values, s7.values, s8.values)))
+    tastes_df = tastes_df.rename(columns={0:'tastes'})
+    tastes_df = tastes_df.sort_values(by='tastes')
+    tastes_df = tastes_df.drop_duplicates(keep='first', inplace=False)
+    tastes_df = tastes_df.reset_index().drop(columns='index')
+
+    # """Return the homepage."""
+    return render_template("taste.html", taste_list=tastes_df['tastes'])
+
+
+@app.route("/taste/<selected_taste>")
+def taste_func(selected_taste):
+
+    conn = 'mongodb://test:password1@ds123444.mlab.com:23444/heroku_3t530jfl'
+    client = pymongo.MongoClient(conn)
+    
+    #extract to DB
+    db = client.heroku_3t530jfl
+    df = pd.DataFrame(list(db.styles.find({})))
+
+    new_df = df[['alcohol', 'brand_name', 'grape_type', 'review', 'wine_style', 'wine_type']]
+      
+    #strip the wine type from the front of the wine style
+    new_df['wine_style'] = new_df['wine_style'].apply(lambda x: x.split(" - ")[-1] if(x) else x)
+        
+    #bring in the intensity collection from Mongo and put in dataframe
+    df_int = pd.DataFrame(list(db.intensity.find({})))
+    df_int = df_int.drop(columns='_id')
+    
+    #the wine_style column header is coming in with garbage characters
+    #use this code to rename it
+    df_int.columns = ['intensity_score', 'wine_style']
+    
+    #strip the wine type from the front of the wine style
+    df_int['wine_style'] = df_int['wine_style'].apply(lambda x: x.split(" - ")[-1] if(x) else x)
+    
+    #merge the wine intensities with the styles data
+    df_with_int = new_df.merge(df_int, on='wine_style')
+    
+    #bring in the tasting notes collection from Mongo and put in dataframe
+    df_taste = pd.DataFrame(list(db.taste.find({})))
+    df_taste = df_taste.drop(columns='_id')
+    
+    #merge again to include the tasting notes in the big dataframe
+    df_merge = df_with_int.merge(df_taste, on='wine_style')
+    
+    #rename the wine_type column
+    df_merge = df_merge.rename(columns={"wine_type_x":"wine_type"})
+    df_merge = df_merge.drop(columns='wine_type_y')
+    
+    #turn the tasting notes column from a list into a string so we can search it
+    df_merge['notes'] = df_merge['tasting_notes'].apply(', '.join)
+    df_merge = df_merge.drop(columns='tasting_notes')
+    
+    #now search the dataframe for the selected tasting note
+    search_df = df_merge[df_merge['notes'].str.contains(selected_taste)]
+    search_df = search_df.sort_values(by=['brand_name'])
+
+    search_df = search_df.reset_index()
+
+    #convert dataframe columns to lists, put into a dictionary and then jsonify
+    alcohol_pct = list(search_df['alcohol'])
+    brand = list(search_df['brand_name'])
+    grape = list(search_df['grape_type'])
+    review = list(search_df['review'])
+    style = list(search_df['wine_style'])
+    wine_type = list(search_df['wine_type'])
+    intensity_score = list(search_df['intensity_score'])
+   
+    table_data = []
+    i=0
+    while i < len(brand):
+
+        dict = {
+        "Brand":brand[i], 
+        "Grape":grape[i],
+        "Wine Type":wine_type[i],
+        "Alcohol %":alcohol_pct[i],
+        "Score":review[i],
+        "Style":style[i],
+        "Intensity":intensity_score[i]    
+        }
+        table_data.append(dict)
+        i+=1
+
+    return jsonify(table_data)
 
 @app.route("/food")
 def foodpage():
